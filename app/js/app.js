b9825fab141b96bd5e23ba27fa7e1bd76538364d
arcelan <lu_zhiyuan@yeah.net>
2017-01-22 14:22:59 +0800

    add commit button

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
     'use strict';
 
-    var UPDATE_INTERVAL = 5000;
+    var UPDATE_INTERVAL = 10000;
     var restApiUrl1 = 'json/data1.json';
     var restApiUrl2 = 'json/data2.json';
     // var restApiUrl='https://f12w3m8f3b.execute-api.us-west-2.amazonaws.com/beta/devicestatus?nsukey=s7GXS572h2AuMjlXwBF23OeZaKe5dmOM5Dq1Pa0i01%2F2FZBGtHcV4A6tS81wMMXF2%2FKrcAVhTQW2D%2FTILVDH850TAW1lqgIJLow6iCuiOEhIEmk9csJmg9rkYrcnXQfY1BVhZHCIOqa%2BG5aBSOivSVgwdiPz%2FCxPS0W6pJCueADGbUqMx%2Bsg1hlUzlwH8N0c';
@@ -30,33 +30,51 @@ jQuery(function ($) {
 
     var app = {
         init: function () {
-            this.infoTemplate = Handlebars.compile($('#info-template').html());
-            this.infoData = [];
+            this.template = Handlebars.compile($('#info-template').html());
+            this.data = [];
+            this.selectedData = '';
+            this.bindEvents();
             this.start();
+        },
+        bindEvents: function () {
+            $('#device-info').on('click', 'tr', this.select.bind(this));
+            $('#device-info').on('change', '.toggle', this.change.bind(this));
         },
         start: function () {
             this.update();
         },
         update: function () {
             var self = this;
-            $("#loader").spin(spinOpts);
+            $('#loader').spin(spinOpts);
             $.getJSON(restApiUrl1).then(function (results) {
-                self.updateInfo(results);
+                self.updateData(results);
                 self.render();
-                $("#loader").spin(false);
+                $('#loader').spin(false);
                 setTimeout(self.update.bind(self), UPDATE_INTERVAL);
             });
         },
-        updateInfo: function (datas) {
+        updateData: function (datas) {
             var self = this;
             var guids = datas.guids;
-            this.infoData = [];
+            this.selectedData = '';
+            this.data = [];
             _.each(guids, function (guid) {
-                self.infoData.push(_.extend({guid: guid}, datas[guid]));
+                self.data.push(_.extend({guid: guid}, datas[guid]));
             });
         },
         render: function () {
-            $('#status-data').html(this.infoTemplate(this.infoData));
+            $('#guid-text').val(this.selectedData);
+            $('#device-info').html(this.template(this.data));
+        },
+        select: function (e) {
+            $(e.currentTarget).parent().find('.info').removeClass('info');
+            $(e.currentTarget).addClass('info');
+            $(e.currentTarget).find('input[type="radio"]').prop('checked', true).change();
+        },
+        change: function (e) {
+            var $selected = $('#device-info').find('input[type="radio"]:checked');
+            this.selectedData = $selected.val();
+            $('#guid-text').val(this.selectedData);
         }
     };
 
